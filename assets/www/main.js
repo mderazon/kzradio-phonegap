d271955cdf97b21adf2572c7d8821b7317cbc699
michael <mderazon@hotmail.com>
2013-01-07 21:48:07 +0200

    working phonegap media example

--- assets/www/main.js
+++ assets/www/main.js
@@ -157,6 +157,27 @@ function toggleCompass() {
     }
 }
 
+// Audio player
+//
+var my_media = new Media(src, onSuccess, onError);
+
+// Update media position every second
+var mediaTimer = setInterval(function() {
+    // get media position
+    my_media.getCurrentPosition(
+        // success callback
+        function(position) {
+            if (position > -1) {
+                console.log((position) + " sec");
+            }
+        },
+        // error callback
+        function(e) {
+            console.log("Error getting pos=" + e);
+        }
+    );
+}, 1000);
+
 function init() {
     // the next line makes it impossible to see Contacts on the HTC Evo since it
     // doesn't have a scroll button
